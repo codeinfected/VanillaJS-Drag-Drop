24a7705fa5a7a518025ebe8e5a8c81fed3e73f2e
Jubair Ahmad <jubairahmad1994@gmail.com>
2020-05-21 21:56:32 +0600

    Working

---
+++ app.js
@@ -0,0 +1,40 @@
+const fill = document.querySelector('.fill');
+// Converting a NodeList into an Array Using ES2015 Spread operator, so that I can use the Array.prototype.map on it
+const empties = [...document.querySelectorAll('.empty')];
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+empties.map((empty) => {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+});
+
+function dragStart() {
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+}
